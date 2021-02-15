8dd2512b5ec8e5712c5ae84a6444fed1d27787e2
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2021-02-15 19:07:17 -0300

    add users

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
@@ -10,6 +10,12 @@ class Appointment {
 
     @Column('timestamp with time zone')
     data: Date;
+
+    @CreateDateColumn()
+    create_at: Date;
+
+    @UpdateDateColumn()
+    update_at: Date;
 }
 
 export default Appointment;
